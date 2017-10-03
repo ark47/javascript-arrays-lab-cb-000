468367f4ce09e83abce5d87a15e37afdb68f279a
Chris Nunez <cenunez47@gmail.com>
2017-10-03 00:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1);
-  return kittens
+  var kitties = kittens.slice(0, kittens.length - 1);
+  return kitties
 }
